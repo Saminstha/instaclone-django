14ad7917ede706cce517d614cb1bf507d0f58303
Samin Shrestha <saminshrestha7@gmail.com>
2026-02-07 21:54:12 +0545

    added views for post create and feedview

--- insta_app/urls.py
+++ insta_app/urls.py
@@ -1,3 +1,8 @@
 from django.urls import path
-from . import views
+from .views import FeedView, ProfileView, CreatePostView
 
+urlpatterns = [
+    path('', FeedView.as_view(), name='feed'),
+    path('profile/<str:username>/', ProfileView.as_view(), name='profile'),
+    path('create/', CreatePostView.as_view(), name='create_post'),
+]
--- insta_app/views.py
+++ insta_app/views.py
@@ -7,3 +7,34 @@ from django.urls import reverse_lazy
 from django.shortcuts import get_object_or_404
 from .models import Post
 
+class FeedView(ListView):
+    model = Post
+    template_name = 'insta_app/feed.html'
+    context_object_name = 'posts'
+    ordering = ['-created_at']
+
+class ProfileView(ListView):
+    model = Post
+    template_name = 'insta_app/profile.html'
+    context_object_name = 'posts'
+
+    def get_queryset(self):
+        self.profile_user = get_object_or_404(
+            User, username=self.kwargs['username']
+        )
+        return Post.objects.filter(user=self.profile_user).order_by('-created_at')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['profile_user'] = self.profile_user
+        return context
+
+class CreatePostView(CreateView):
+    model = Post
+    fields = ['image', 'caption']
+    template_name = 'insta_app/create_post.html'
+    success_url = reverse_lazy('feed')
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
